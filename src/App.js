a2e663b055ca52e6491450b2a6c718b28487c465
rafael-feliciano <rafael.cefetpg@gmail.com>
2020-08-19 15:59:37 -0300

    feat:[200~

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      
+       
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+              <Menu.Item key="1">Nome Usuário</Menu.Item>
+              <Menu.Item key="2">Login</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+           
+            <div className="site-layout-content">Formulario</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Kenzie</Footer>
+          
+        </Layout>
+    
     </div>
   );
 }
