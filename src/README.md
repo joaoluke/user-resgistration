1cced2fef30b664bab63e59e06b9752466447d07
rafael-feliciano <rafael.cefetpg@gmail.com>
2020-08-20 22:15:45 -0300

    test

---
+++ src/README.md
@@ -0,0 +1,2 @@
+https://gitlab.com/kenzie-academy-brasil/demos/2020-abr-q2-projects/users-api     // API
+https://github.com/axios/axios     //documentação e codigo axios
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,6 @@
 // https://ant.design/components/form/  formulario Log in
-import React from 'react';
+import React, { useState } from 'react';
+
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
@@ -7,9 +8,33 @@ import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './index.css'
 
+import axios from 'axios';
+
 const Login = () => {
-  const onFinish = values => {
+
+  const [token, setToken] = useState(localStorage.getItem("token")); 
+  const [requestError, setRequestError] = useState("")
+
+  const onFinish = (values) => {
     console.log('Received values of form: ', values);
+    axios     // se login for sucesso executa o then, se possui erro executa o catch
+      .post("https://ka-users-api.herokuapp.com/authenticate",values) //rafa
+      .then((res) => {
+        console.log(res)
+        setToken(res.auth_token);
+        localStorage.setItem("token", res.auth_token)
+        setRequestError("")
+      })
+      
+      .catch((error) => { // catch - Trata os erros
+        //debugger;
+        console.log(error.response.data); //retorna o erro especifico, error é um objeto
+        console.log(error.message) // mensagem generica erro 
+        console.log(error.response.status)  // ex. erro 401
+        if (error.response.status === 401){
+          setRequestError("Credenciais Inválidas")
+        }else setRequestError ("Erro requisição")
+      })
   };
 
   return (
@@ -22,7 +47,7 @@ const Login = () => {
       onFinish={onFinish}
     >
       <Form.Item
-        name="username"
+        name="user"
         rules={[
           {
             required: true,
@@ -54,8 +79,25 @@ const Login = () => {
         <Button type="primary" htmlType="submit" className="login-form-button">
           Log in
         </Button>
+      </Form.Item>
+
+
+
+
+
+      <Form.Item>
+       
+        <div> {requestError} </div>
+        
        
       </Form.Item>
+
+
+
+
+
+
+      
       
     </Form>
   );
