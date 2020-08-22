950ce8519719af2ba457f9916f58106287e8788a
rafael-feliciano <rafael.cefetpg@gmail.com>
2020-08-22 12:25:36 -0300

    id loading on the page

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -16,8 +16,8 @@ const Users = () => {
       { headers: {Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1MzYsImV4cCI6MTYyOTU1ODkwNH0.g85VIjVCsBZ2DmtQY6JC4Difliypm7fi9R5Obe7FZbg"} })
 
     .then(response => { //request bem sucedido
-              console.log("GET")
-              console.log(response.data)
+              // console.log("GET")
+               console.log(response.data)
               setTeste(response.data)
             
             })
@@ -29,54 +29,11 @@ const Users = () => {
   const columns = [
         {
             title: "nome",
-            dataIndex: "name", //id, nome
-            key: 'name',
+            dataIndex: "id", //id, nome
+            key: 'id',
             render: text => <a>{text}</a>,
         },
-
-        // {
-        //     title: 'Age',
-        //     dataIndex: 'age',
-        //     key: 'age',
-        // },
-
-        // {
-        //     title: 'Address',
-        //     dataIndex: 'address',
-        //     key: 'address',
-        // },
-
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: tags => (
-            <>
-            {tags.map(tag => {
-                  let color = tag.length > 5 ? 'geekblue' : 'green';
-                  if (tag === 'loser') {
-                      color = 'volcano';
-                  }
-                  return (
-                      <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                      </Tag>
-                  );
-            })}
-              </>
-              ),
-        },
-
-        {
-            title: 'Action',
-            key: 'action',
-            render: (text, record) => (
-            <Space size="middle">
-                  <a>Invite {record.name}</a>
-                  <a>Delete</a>
-            </Space>
-            ),
-        },
+       
   ];
 
   const data = [
@@ -88,26 +45,12 @@ const Users = () => {
             address: 'New York No. 1 Lake Park',
             tags: ['nice', 'developer'],
         },
-
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sidney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
   ];
 
-  return <Table dataSource={data} columns={columns} />;
-     
+  return  (
+    
+    <Table dataSource={teste} columns={columns} />
+  )
 }
 
 export default Users;
